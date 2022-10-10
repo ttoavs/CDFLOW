a18a4981b359e2f978ab67a8e8bc476ec954cf0f
ttoavs <95141573+ttoavs@users.noreply.github.com>
2022-10-10 13:54:35 -0500

    Add files via upload

---
+++ Main/0_Prep.R
@@ -0,0 +1,12 @@
+# Run this script first to create emty folder to add data to complete this project
+
+dir.create("Data")
+
+dir.create("Data/CreatedData")
+dir.create("Data/RawData")
+
+dir.create("Data/RawData/Chemical")
+dir.create("Data/RawData/Partial Site")
+dir.create("Data/RawData/Site")
+
+dir.create("Supplementary/GroundtruthPlots")
