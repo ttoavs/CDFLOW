c72a641b81e1a5ac7080dd4b92e6b45f143f691d
ttoavs <95141573+ttoavs@users.noreply.github.com>
2023-09-12 12:10:04 -0500

    final

---
+++ database-compilation-scripts/B5-CDFLOW.R
@@ -0,0 +1,20 @@
+library(tidyverse)
+
+estimates <- read_csv("Data/Created/pCO2.csv")
+sites <- read_csv("Data/Created/Site-Lookup.csv")
+obsv <- read_csv("Data/Created/Raw-Obsv.csv")
+
+
+obsv <- obsv %>%
+  select(obs_id, ActivityStartDate, ActivityStartTime.Time, `pH.std units`, `Temperature, water.deg C`, `Alkalinity.ueq/kgw`, State)
+
+sites <- sites %>%
+  select(-MLI_Long, -MLI_Lat)
+
+CDFLOW <- merge(estimates, obsv, by = "obs_id")
+CDFLOW <- merge(CDFLOW, sites, by = "MonitoringLocationIdentifier")
+
+CDFLOW <- CDFLOW %>%
+  select(-obs_id)
+
+write.csv(CDFLOW, "CDFLOW.csv", row.names = FALSE)
